7870d168d0c484e8fb0370faf2e05aa200f3ada0
Mahadev-10-18 <gvsmahadev@gmail.com>
2025-09-09 21:34:03 +0530

    reverseinteger

--- bubblesort_day1.c
+++ bubblesort_day1.c
@@ -5,10 +5,8 @@
 void bubbleSort(int arr[], int n) {
     int i, j, temp;
     for (i = 0; i < n - 1; i++) {
-        // Last i elements are already sorted
         for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
+            if (arr[j] > arr[j + 1]){
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
